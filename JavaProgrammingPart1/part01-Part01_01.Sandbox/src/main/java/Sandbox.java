5851fe6409e5b5748f27cc65cf9f3b6e7c76c3bb
janlouie30 <139190332+janlouie30@users.noreply.github.com>
2024-03-22 07:58:35 +0800

    Update Sandbox.java

--- JavaProgrammingPart1/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
+++ JavaProgrammingPart1/part01-Part01_01.Sandbox/src/main/java/Sandbox.java
@@ -1,14 +1,6 @@
-
-import java.util.Scanner;
-
-
 public class Sandbox {
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
         System.out.println("Hello World!");
-        System.out.print("From where? ");
-        int start = Integer.valueOf(scanner.nextLine());
     }
 }
